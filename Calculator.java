f1d4ae327b4bfa021f9766eab3ab83226827d784
Pulkit Shrivastava <167713133+pulkit100804@users.noreply.github.com>
2025-04-19 23:21:43 +0530

    added substract to the calculator Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -10,3 +10,13 @@ public class Calculator {
     public static double add(double a, double b) {
         return a + b;
     }
+  /**
+     * Subtracts second number from the first.
+     *
+     * @param a first number
+     * @param b second number
+     * @return difference of a and b
+     */
+    public static double subtract(double a, double b) {
+        return a - b;
+    }
